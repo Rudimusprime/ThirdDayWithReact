c96d20303ac0b1d9ec2b68b4f5b3433a06a6295e
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-08 20:43:44 +0200

    button handler with parameters done

--- FirstExample/scripts/App.js
+++ FirstExample/scripts/App.js
@@ -1,15 +1,45 @@
 class Counter extends React.Component {
+    state = {
+        count: 0,
+        result: this.props.result,
+    }
+
+    handleMathClick(type, number = 1) {
+        debugger
+        if (type === "substraction") {
+            this.setState(prevState => ({
+                    count: prevState.count + 1,
+                    result: prevState.result - number,
+                })
+            )
+        } else if (type === "reset") {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                result: 0,
+            }))
+        } else if (type === "addition") {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                result: prevState.result + number,
+            }))
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
-                <button>-1</button>
-                <button>Reset</button>
-                <button>+1</button>
-                <h1>Liczba kliknięć:</h1>
-                <h1>Wynik:</h1>
+                <button onClick={this.handleMathClick.bind(this, "substraction", 10)}>-10</button>
+                <button onClick={() => this.handleMathClick("substraction")}>-1</button>
+                <button onClick={() => this.handleMathClick("reset")}>Reset</button>
+                <button onClick={() => this.handleMathClick("addition")}>+1</button>
+                <button onClick={() => this.handleMathClick("addition", 10)}>+10</button>
+                <h1>Liczba kliknięć: {this.state.count}</h1>
+                <h1>Wynik: {this.state.result}</h1>
             </React.Fragment>
         )
     }
 }
 
-ReactDOM.render(<Counter/>, document.getElementById('root'));
+const startValue = 10;
+
+ReactDOM.render(<Counter result={startValue}/>, document.getElementById('root'));
